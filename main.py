31ccca37843b6cf8bed0633d9d840ba1298823ac
pmo001 <pmo001@ucr.edu>
2019-11-05 20:59:35 -0800

    making a checkpoint here

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import heapq #ref: inspired from Lorem Ipsum project sample
+import copy #ref: also inspired from sample project
 
 #general srch alg: (from project briefing)
 #arg1: problem(init state)
 #arg2: queueing-function
 #function srch_1,2or3(arg1, arg2):
-#nodes = make node init state and enqueue node init state 
+#nodes = create the node:init state
+#  then enqueue init node as root in minheap 
 #loop do
 #   if init state not valid(empty or impossible), return "failure"
 #   if state == goal, return (node?)
@@ -63,27 +65,97 @@ def calc_h_manhattan_dist(puzl_list):
 #ref: consulted https://www.tutorialspoint.com/python/python_nodes.htm
 #       on how to build node class
 #creating nodes class that rep puzl matrices and val = g + h
+#how2use: insert g+h as arg to get total val
+#purpose: creates a trace from root to goal node + respective cost vals
 class puzl_node:
-    def __init__(self, dataval=None):
-        self.dataval = dataval #the arg is the list(matrix)
-        self.nextval = None #points to nothing as of now
+    def __init__(self, mtx, g_val=0, h_val=0):
+        self.mtx = copy.deepcopy(mtx) #deep copies the mtx(list)
+        self.g_val = g_val
+        self.h_val = h_val
+        self.f_val = self.g_val + self.h_val
+        #fixme: need parent? maybenot?
+        self.parent = None
+        self.nextNode = None
+        
+        #fixme: rm: points to list(matrix) where 0 moves up
 
-def minheap_impl():
+        #self.nextNode_down = None #" " where 0 moves down
+
+#TODO: implement formula for g
+#fixme? is this what I want? function: the curr cost of curr node path
+#
+#fixme? should up/down moves cost 2 while L/R cost 1?
+
+#first impl: each move = 1
+#arg = node
+# returns node's g_val
+#fixme: might not need this. could just incr directly in the node
+def calc_g(node):
+    node.g_val += 1 #incr g_val by one after a move is made
+    return
+
+#checks whether curr node is the goal state
+#if heuristic == manhattan, goal == if h(n) == 0
+#if heuristic == hamming, goal == if h(n) == 0
+#if using uniform cost fcn: must check if each tile is in correct place
+#arg: node.val which contains the matrix(list)
+def goal_test(node):
+    if (heuristic == "manhattan" and node.h_val == 0):
+        return True
+    else:
+        return False
+
+#arg: init_node = a node from main()
+def a_star(init_node):
     minheap = [] #init minheap
+    #2nd arg: a tuple of f(n) and the deepcopied mtx
+    #minheap: the lowest f_val is the root
+    heapq.heappush(minheap, (init_node.f_val, init_node)) #fixme? make get_f_val?
 
+    #while(bool_not_goal_state)
+    #   1. assign parent variable=root node
+    #   2.insert nodes of next states' possible moves (up,down,L,R)
+    #       >first copy parent node, then make alteration
+    #       >the minheap will auto move the lowest_val_node into root
+    #   3.assign 
+    #   4.pop the root node
+    #       the non-root node with the lowest val gets moved to top of minheap
+    #       check if root==goal state
+    #       print root
+    #       keep not_goal_state true
+    #return to top of while(not_goal_state) loop and do all over again until not_goal_state==false
+    
+    return
+
+#global variables:
+heuristic = "manhattan"
 
 def main():
+#tests:
     #h_md_sample_sol = calc_h_manhattan_dist(sample_sol)
     #print("solution = 3: ", h_md_sample_sol)
+
     #h_md_trivial = calc_h_manhattan_dist(trivial)
 #    print("solution = 0: ", h_md_trivial)
+    ###trivial_mtx_node = puzl_node(trivial) #inserts trivial mtx into initial_state node
+    ###trivial_mtx_node.h_val = calc_h_manhattan_dist(trivial)
+    ###a_star(trivial_mtx_node)
+    ###if goal_test()
+
  #   h_md_oneAway = calc_h_manhattan_dist(one_away)
   #  print("solution = 1: ", h_md_oneAway)
+
+    #1st arg: mtx, 2nd arg: g, 3rd arg: h
+    mtx_node = puzl_node(one_away, 0, calc_h_manhattan_dist(one_away))
+    print("goal test should be false at this point: ", goal_test(mtx_node)) 
+
+    ###a_star(mtx_node)
+    ###if goal_test()
+
    # h_md_mustTrace = calc_h_manhattan_dist(must_trace)
     #print("solution = 2: ", h_md_mustTrace)
 
     return
-
 #python3: don't need if__name
 main()
 
