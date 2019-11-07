dc1b962b6e549f1d8ecf88a5f3b79a05b50c734a
pmo001 <pmo001@ucr.edu>
2019-11-06 19:43:09 -0800

    savepoint

--- main.py
+++ main.py
@@ -52,10 +52,6 @@ goal_dict = {1:[0,0], 2:[0,1], 3:[0,2], 4:[1,0], 5:[1,1], 6:[1,2],
             7:[2,0], 8:[2,1]}
 
 #a list of tuples of moves the 0 can do in a matrix
-#U: 11-> 01
-#D: 01-> 11
-#L: 11-> 10
-#R: 11-> 12
 add_move = [('U', (-1,0)), 
             ('D', (1,0)),
             ('L', (0,-1)),
@@ -72,6 +68,7 @@ def set_heuristic(userInput):
     global_heuristic = userInput
     return
 
+
 #ref: consulted https://www.tutorialspoint.com/python/python_nodes.htm
 #       on how to build node class
 #creating nodes class that rep puzl matrices and val = g + h
@@ -86,8 +83,9 @@ class puzl_node:
         self.zero_pos = locate_zero_pos(self.mtx)
         #fixme: need parent? maybenot?
         self.parent = None
-        self.nextNode = None
+        self.nextNode = None #fixme: i don't think i need to implement this, parent is enuf
         
+        self.numNodes = 0 #fixme? counts numNodes created
         #fixme: rm: points to list(matrix) where 0 moves up
 
         #self.nextNode_down = None #" " where 0 moves down
@@ -115,7 +113,9 @@ def locate_zero_pos(mtx):
                 return (y,x)
 
 #to call this, 1st arg: node
-def possible_mtx_nodes(node):
+#2nd arg: minheap
+#TODO: make it so that I can add each new node made in the for loop to the minheap
+def add_poss_mtx_nodes(node, minheap):
     #fixme: change if to for
     
     print("within poss_mtx_node; printing orig mtx:")
@@ -145,48 +145,14 @@ def possible_mtx_nodes(node):
                                         (node.g_val)+1,
                                         calc_h_manhattan_dist(new_mtx),
                                         (new_y_zero_pos, new_x_zero_pos))
-
-        print("printing node's new mtx:   ")
-        print_nodes_mtx(new_mtx_node)
-        print("                  <<<<<<<<<<<<<<<<<<<<")
+            #assign parent pointer to prev
+            new_mtx_node.parent = node
+            new_mtx_node.numNodes += 1
+            #>>add the new node to minheap (still within if-statement)
+            #add to minheap by priority(f-val)
+            heapq.heappush(minheap, (new_mtx_node.f_val, new_mtx_node))
+        #print_nodes_mtx(new_mtx_node)
     return
-
-#h(n) = heuristic that /underestimates/
-#   the sum of /each/ tile's_distance_to_desired_pos
-#purpose: add this with g(n)=cost_of_path2currNode
-#   g+h = f, use f to determine which node to expand
-def calc_h_manhattan_dist(puzl_list):
-    sum = 0
-    for y in range(len(puzl_list)): #rowNum 0,1,2
-        for x in range(len(puzl_list[0])):
-            if puzl_list[y][x] != 0: #0 has no dict
-                yIndx_of_goal, xIndx_of_goal = goal_dict.get(puzl_list[y][x])
-                #formula reference @ line21,22
-                sum += abs(yIndx_of_goal - y) + abs(xIndx_of_goal - x)
-    return sum
-
-#checks whether curr node is the goal state
-#1.if heuristic == manhattan, goal == if h(n) == 0
-# if heuristic == hamming, goal == if h(n) == 0
-#2.if using uniform cost fcn: must check if each tile is in correct place
-#arg: node.val which contains the matrix(list)
-#todo: impl 2 and 3
-def goal_test(node):
-    if ((global_heuristic in list_heuristic) and node.h_val == 0):
-        return True
-    elif global_heuristic == "uniform cost" and equal_trivial(node.mtx):
-        return True
-    else:
-        return False
-
-#compares arg_mtx with trivial: tile by tile
-def equal_trivial(mtx):
-    for y in range(len(trivial)):
-        for x in range(len(trivial[0])):
-            if mtx[y][x] != trivial[y][x]:
-                return False
-    return True
-
 
 #arg: init_node = a node from main()
 def a_star(init_node):
@@ -208,23 +174,64 @@ def a_star(init_node):
     #       print root
     #       keep not_goal_state true
     #return to top of while(not_goal_state) loop and do all over again until not_goal_state==false
-    
+    bool_not_goal_state = True
+    while(bool_not_goal_state):
+
+        #step2: within while-loop: insert new nodes
+        add_poss_mtx_nodes(minheap)
+        #all poss new nodes in minheap as of now
+
+        #step3: take out of root and check with seen(set)
+        #if seen, continue
+        #else, add to seen
+#todo: where to impl. this?: #check if goal-state: yes?-> bool = False
+            #todo: then start a minheap with newly taken out node?
+
+
     return
 
-#global variables:
-global_heuristic = "manhattan"
+#TODO: impl. print path from goal state back up to root using parent var
+
+#h(n) = heuristic that /underestimates/
+#   the sum of /each/ tile's_distance_to_desired_pos
+#purpose: add this with g(n)=cost_of_path2currNode
+#   g+h = f, use f to determine which node to expand
+def calc_h_manhattan_dist(puzl_list):
+    sum = 0
+    for y in range(len(puzl_list)): #rowNum 0,1,2
+        for x in range(len(puzl_list[0])):
+            if puzl_list[y][x] != 0: #0 has no dict
+                yIndx_of_goal, xIndx_of_goal = goal_dict.get(puzl_list[y][x])
+                #formula reference @ line21,22
+                sum += abs(yIndx_of_goal - y) + abs(xIndx_of_goal - x)
+    return sum
+
+#1.if heuristic == manhattan, goal == if h(n) == 0
+# if heuristic == hamming, goal == if h(n) == 0
+#2.if using uniform cost fcn: must check if each tile is in correct place
+def goal_test(node):
+    if ((global_heuristic in list_heuristic) and node.h_val == 0):
+        return True
+    elif global_heuristic == "uniform cost" and equal_trivial(node.mtx):
+        return True
+    else:
+        return False
+
+#compares arg_mtx with trivial: tile by tile
+def equal_trivial(mtx):
+    for y in range(len(trivial)):
+        for x in range(len(trivial[0])):
+            if mtx[y][x] != trivial[y][x]:
+                return False
+    return True
+
+
+#TODO: set global_heuristic = "manhattan" or whatever user wants
 
 def main():
 #tests:
     #h_md_sample_sol = calc_h_manhattan_dist(sample_sol)
     #print("solution = 3: ", h_md_sample_sol)
-
-    #h_md_trivial = calc_h_manhattan_dist(trivial)
-#    print("solution = 0: ", h_md_trivial)
-    ###trivial_mtx_node = puzl_node(trivial) #inserts trivial mtx into initial_state node
-    ###trivial_mtx_node.h_val = calc_h_manhattan_dist(trivial)
-    ###a_star(trivial_mtx_node)
-    ###if goal_test()
 
  #   h_md_oneAway = calc_h_manhattan_dist(one_away)
   #  print("solution = 1: ", h_md_oneAway)
@@ -234,20 +241,12 @@ def main():
     #print("goal test should be false at this point: ", goal_test(mtx_node)) 
     #print("testing matrix==trivial:", equal_trivial(trivial))
     #print(print_mtx(center))
-    #print(locate_zero_pos(center))
-    #print(mtx_node.zero_pos)
-    possible_mtx_nodes(mtx_node)
 
 
     ###a_star(mtx_node)
-    ###if goal_test()
 
    # h_md_mustTrace = calc_h_manhattan_dist(must_trace)
     #print("solution = 2: ", h_md_mustTrace)
 
     return
-#python3: don't need if__name
 main()
-
-#stick init state list into minheap
-#expand depending on g+h
